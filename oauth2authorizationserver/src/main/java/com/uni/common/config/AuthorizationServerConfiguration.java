2bf49289061f62ed13c35535fea36a0f31501c73
LiuQiV5 <1504885830@qq.com>
2021-01-03 14:54:00 +0800

    认证授权服务器

---
+++ oauth2authorizationserver/src/main/java/com/uni/common/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,173 @@
+package com.uni.common.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    AuthenticationManager authenticationManager;
+
+    KeyPair keyPair;
+
+    boolean jwtEnabled;
+
+    public AuthorizationServerConfiguration(
+            AuthenticationConfiguration authenticationConfiguration,
+            KeyPair keyPair,
+            @Value("${security.oauth2.authorizationserver.jwt.enabled:true}") boolean jwtEnabled) throws Exception {
+
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+        this.keyPair = keyPair;
+        this.jwtEnabled = jwtEnabled;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+        // @formatter:off
+        clients.inMemory()
+                .withClient("reader")
+                .authorizedGrantTypes("password")
+                .secret("{noop}secret")
+                .scopes("message:read")
+                .accessTokenValiditySeconds(600_000_000)
+                .and()
+                .withClient("writer")
+                .authorizedGrantTypes("password")
+                .secret("{noop}secret")
+                .scopes("message:write")
+                .accessTokenValiditySeconds(600_000_000)
+                .and()
+                .withClient("noscopes")
+                .authorizedGrantTypes("password")
+                .secret("{noop}secret")
+                .scopes("none")
+                .accessTokenValiditySeconds(600_000_000);
+        // @formatter:on
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        // @formatter:off
+        endpoints
+                .authenticationManager(this.authenticationManager)
+                .tokenStore(tokenStore());
+
+        if (this.jwtEnabled) {
+            endpoints
+                    .accessTokenConverter(accessTokenConverter());
+        }
+        // @formatter:on
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        if (this.jwtEnabled) {
+            return new JwtTokenStore(accessTokenConverter());
+        } else {
+            return new InMemoryTokenStore();
+        }
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(this.keyPair);
+
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+        accessTokenConverter.setUserTokenConverter(new SubjectAttributeUserTokenConverter());
+        converter.setAccessTokenConverter(accessTokenConverter);
+
+        return converter;
+    }
+
+    @FrameworkEndpoint
+    class IntrospectEndpoint {
+        TokenStore tokenStore;
+
+        IntrospectEndpoint(TokenStore tokenStore) {
+            this.tokenStore = tokenStore;
+        }
+
+        @PostMapping("/introspect")
+        @ResponseBody
+        public Map<String, Object> introspect(@RequestParam("token") String token) {
+            OAuth2AccessToken accessToken = this.tokenStore.readAccessToken(token);
+            Map<String, Object> attributes = new HashMap<>();
+            if (accessToken == null || accessToken.isExpired()) {
+                attributes.put("active", false);
+                return attributes;
+            }
+
+            OAuth2Authentication authentication = this.tokenStore.readAuthentication(token);
+
+            attributes.put("active", true);
+            attributes.put("exp", accessToken.getExpiration().getTime());
+            attributes.put("scope", accessToken.getScope().stream().collect(Collectors.joining(" ")));
+            attributes.put("sub", authentication.getName());
+
+            return attributes;
+        }
+    }
+
+    @FrameworkEndpoint
+    class JwkSetEndpoint {
+        KeyPair keyPair;
+
+        JwkSetEndpoint(KeyPair keyPair) {
+            this.keyPair = keyPair;
+        }
+
+        @GetMapping("/.well-known/jwks.json")
+        @ResponseBody
+        public Map<String, Object> getKey() {
+            RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+            RSAKey key = new RSAKey.Builder(publicKey).build();
+            return new JWKSet(key).toJSONObject();
+        }
+    }
+
+    class SubjectAttributeUserTokenConverter extends DefaultUserAuthenticationConverter {
+        @Override
+        public Map<String, ?> convertUserAuthentication(Authentication authentication) {
+            Map<String, Object> response = new LinkedHashMap<>();
+            response.put("sub", authentication.getName());
+            if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()) {
+                response.put(AUTHORITIES, AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
+            }
+            return response;
+        }
+    }
+}
